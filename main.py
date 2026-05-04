948ee80f2bd3e85b223141e6035ce16c5f012137
caferayguen22-commits <cafer.ayguen22@gmail.com>
2026-05-04 14:24:30 +0200

    verbessere HTML der Karten_Elemente

--- main.py
+++ main.py
@@ -16,29 +16,36 @@ for animal in animals_data:
     # Name hinzufügen
     output += f" <div class='card__title'>{animal.get('name')}</div><br/>\n"
 
+    output += ' <p class="card__text">\n'
+
     # Details hinzufügen
     characteristics = animal.get("characteristics", {})
+
+    # Ernährung
     if "diet" in characteristics:
         output += f" <strong>DIET:</strong> {characteristics.get('diet')}<br/>\n"
-
+    # Ort
     locations = animal.get("locations")
     if locations:
         output += f" <strong>Location:</strong> {locations[0]}<br/>\n"
 
+    # Typ
     if "type" in characteristics:
         output += f" <strong>Type: </strong> {characteristics['type']}<br/>\n"
 
+    output += "</p>\n"
     output += "</li>\n"
 
-
+# Template lesen und Platzhalter ersetzen
 with open("animals_template.html", "r") as file:
     template_content = file.read()
 
 final_html = template_content.replace("__REPLACE_ANIMALS_INFO__", output)
 
+# In die finale Datei schreiben
 with open("animals.html", "w") as file:
     file.write(final_html)
 
-print("Prüfe die 'animals.html' im Browser.")
+print("Das endgültige Design wurde in 'animals.html' gespeichert.")
 
 
